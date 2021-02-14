06d3a4b1de188a6380041ecc79fd136c0843ad77
Luiz Gabriel Rebelatto <lbrebelatto@gmail.com>
2021-02-13 23:52:50 -0300

    commit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native'
-import Routes from './Routes';
+import Routes from './Routes.js';
 
 export default function App() {
   return (
